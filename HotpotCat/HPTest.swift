c8646a7ba709c73724d5e7f8a6a6eceda8c34712
zhangpeng <zhangpeng4@haier.com>
2021-03-18 11:03:57 +0800

    init
    Change-Id: I4c25531eb28ca4b7dd4998e41091b8e26b5b06bb

---
+++ HotpotCat/HPTest.swift
@@ -0,0 +1,14 @@
+//
+//  HPTest.swift
+//  HotpotCat
+//
+//  Created by ZP on 2021/3/18.
+//
+
+import Foundation
+
+open class HPTest {
+    open func hpTest() {
+        print("Test XC Framework")
+    }
+}
---
+++ HotpotCat/HotpotCat.h
@@ -0,0 +1,18 @@
+//
+//  HotpotCat.h
+//  HotpotCat
+//
+//  Created by ZP on 2021/3/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HotpotCat.
+FOUNDATION_EXPORT double HotpotCatVersionNumber;
+
+//! Project version string for HotpotCat.
+FOUNDATION_EXPORT const unsigned char HotpotCatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HotpotCat/PublicHeader.h>
+
+
